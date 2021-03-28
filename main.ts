1bd2069540dd2056642abb8c32d2c63842d24794
thisisommore <ommore501@gmail.com>
2021-03-28 12:00:20 +0530

    Move files to src folder

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
-import express from 'express'
+import express from "express";
 import cors from "cors";
-import userRoutes from "./routes/userRoutes";
+import userRoutes from "./src/routes/userRoutes";
 
-var app = express()
-var bodyparser = require('body-parser')
-app.use(bodyparser.json())
-app.use(cors())
-app.get("/",(_,res)=>{
-    res.send("<h4>Wrong</h4>")
-})
+var app = express();
+var bodyparser = require("body-parser");
+app.use(bodyparser.json());
+app.use(cors());
+app.get("/", (_, res) => {
+  res.send("<h4>Wrong</h4>");
+});
 
-app.use("/user",userRoutes);
-app.listen(3000,()=>{
-    console.log("Server running");
-})
+app.use("/user", userRoutes);
+app.listen(3000, () => {
+  console.log("Server running");
+});
