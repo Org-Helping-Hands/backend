e8f51e10be3c00127a07c6a0b5d881f28a5a17d1
Om More <51229945+thisisommore@users.noreply.github.com>
2021-06-01 08:25:00 +0530

    TypeOrm: Switch config to typeorm.ts file

--- src/typeorm.ts
+++ src/typeorm.ts
@@ -1,3 +1,20 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-createConnection();
+createConnection({
+  type: "mysql",
+  host: "mysql-service",
+  port: 3306,
+  username: "Om",
+  password: "0000",
+  database: "helpinghands",
+  synchronize: true,
+  logging: false,
+  entities: ["src/models/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/models",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+});
