c2661c5de8d339907a1e9f66af010f2e4c3e602f
Om More <51229945+thisisommore@users.noreply.github.com>
2021-07-30 15:43:02 +0530

    common(mail.ts): Use mailjet

--- src/common/mail.ts
+++ src/common/mail.ts
@@ -1,17 +1,21 @@
 import nodemailer from "nodemailer";
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAILID,
-    pass: process.env.EMAILPASS,
-  },
-});
+const smtp = require("nodemailer-smtp-transport");
+var transporter = nodemailer.createTransport(
+  smtp({
+    host: "in.mailjet.com",
+    port: 2525,
+    auth: {
+      user: process.env.MAILJET_USER,
+      pass: process.env.MAILJET_PASSWORD,
+    },
+  })
+);
 
 export function sendMail(subject: string, text: string, to: string) {
   return new Promise((resolve, reject) => {
     transporter.sendMail(
       {
-        from: process.env.EMAILID,
+        from: "helpinghands163a@gmail.com",
         to,
         subject,
         text,
